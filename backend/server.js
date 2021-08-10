5b6bb970df4e4eb06c93e1255e19b16dcee1eb4f
Gilad <gilad18587@gmail.com>
2021-08-10 13:52:13 +0300

    server established and Heroku setting

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+const path = require("path");
+
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+const serverRoute = require("./server/routes/routes");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/server/api", serverRoute);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.d82yt.mongodb.net/nextDev`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => {
+    console.log("database connect");
+  });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+app.listen(port, () => {
+  return console.log(`application start`);
+});
---
+++ backend/server/model/user.model.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const validator = require("validator");
+
+const userSchema = mongoose.Schema({});
