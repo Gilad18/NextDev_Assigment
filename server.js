4967e05e1268be0461fe2632a29f7917e514cd73
Gilad <gilad18587@gmail.com>
2021-08-11 21:25:13 +0300

    path changed

--- server.js
+++ server.js
@@ -31,13 +31,20 @@ mongoose
   });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static(path.join(__dirname, "./frontend/build")));
 
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  app.get("*", function (_, res) {
+    res.sendFile(
+      path.join(__dirname, "./frontend/build/index.html"),
+      function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
   });
 }
+
 app.listen(port, () => {
   return console.log(`application start`);
 });
